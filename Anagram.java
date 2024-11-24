80656fe1136d0a34d15692030fb57d7d68786b29
talzantkeren <tal.zantkeren@post.runi.ac.il>
2024-11-24 17:26:14 +0200

    fdf

--- Anagram.java
+++ Anagram.java
@@ -8,7 +8,7 @@ public class Anagram {
 		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
 
 		// Tests the preProcess function.
-		System.out.println(preProcess("What? No way!!!"));
+		/*System.out.println(preProcess("What? No way!!!"));
 		
 		// Tests the randomAnagram function.
 		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
@@ -23,27 +23,123 @@ public class Anagram {
 			pass = pass && isAnagram(str, randomAnagram);
 			if (!pass) break;
 		}
-		System.out.println(pass ? "test passed" : "test Failed");
+		System.out.println(pass ? "test passed" : "test Failed");*/
 	}  
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		int len1 = str1.length();
+		int len2 = str2.length();
+		int countLet1 = 0;
+		int countLet2 = 0;
+		int i = 0;
+		int b = 0;
+		int a1 = 0, a2 = 0;
+		for (i = 0; i < len1; i++) {
+			if ((str1.charAt(i) >= 'a' && str1.charAt(i) <= 'z') || 
+				(str1.charAt(i) >= 'A' && str1.charAt(i) <= 'Z')) {
+				a1++;
+			}
+		}
+		for (i = 0; i < len2; i++) {
+			if ((str2.charAt(i) >= 'a' && str2.charAt(i) <= 'z') || 
+				(str2.charAt(i) >= 'A' && str2.charAt(i) <= 'Z')) {
+				a2++;
+			}
+		}
+	
+		if (a1 != a2) {
+			return false;
+		}
+	
+		i = 0; 
+		while (i < len1) {
+			if (!((str1.charAt(i) >= 'a' && str1.charAt(i) <= 'z') || 
+				  (str1.charAt(i) >= 'A' && str1.charAt(i) <= 'Z'))) {
+				i++;
+				continue;
+			}
+	
+			countLet1 = 0;
+			countLet2 = 0;
+	
+			for (b = 0; b < len1; b++) {
+				if (str1.charAt(b) == str1.charAt(i) || 
+					(str1.charAt(b) >= 'a' && str1.charAt(b) == str1.charAt(i) + 32) || 
+					(str1.charAt(b) >= 'A' && str1.charAt(b) == str1.charAt(i) - 32)) {
+					countLet1++;
+				}
+			}
+	
+			for (b = 0; b < len2; b++) {
+				if (str2.charAt(b) == str1.charAt(i) || 
+					(str2.charAt(b) >= 'a' && str2.charAt(b) == str1.charAt(i) + 32) || 
+					(str2.charAt(b) >= 'A' && str2.charAt(b) == str1.charAt(i) - 32)) {
+					countLet2++;
+				}
+			}
+	
+			if (countLet1 != countLet2) {
+				return false;
+			}
+	
+			i++;
+		}
+	
+		return true;
 	}
+	
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		int length= str.length();
+		char []	allc = new char[length];
+		int i = 0;
+		String str1="";
+		for(i=0;i<length;i++){
+			if((str.charAt(i)>= 'a' && str.charAt(i)<= 'z')||(str.charAt(i) == 32)){
+				allc[i]=str.charAt(i);
+			}
+			else{if((str.charAt(i)>= 'A' && str.charAt(i)<= 'Z')){
+					allc[i]=(char)(str.charAt(i)+32);
+					}
+				
+			else{
+					allc[i]=34;
+				}
+			}
+		}
+		for(i=0;i<length;i++){
+			if(allc[i]!=34)
+			{
+				str1+=allc[i]+"";
+		    }
+		}
+		return str1;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		int length = str.length();
+		char [] allc = new char[length];
+		int i = 0;
+		String s="";
+		for(i=0;i<length;i++){
+			allc[i]=str.charAt(i);
+		}
+		for(i=0;i<length-1;i++){
+			int random1 = (int)(Math.random() * length);
+			int random2 = (int)(Math.random() * length);
+			char c= allc[random1];
+			allc[random1]=allc[random2];
+			allc[random2]=c;
+		}
+		for(i=0;i<length;i++){
+			s+=allc[i];
+		}
+		return s;
 	}
 }
--- LoanCalc.java
+++ LoanCalc.java
@@ -23,13 +23,19 @@ public class LoanCalc {
 		System.out.print("\nPeriodical payment, using bi-section search: ");
 		System.out.println((int) bisectionSolver(loan, rate, n, epsilon));
 		System.out.println("number of iterations: " + iterationCounter);
+
 	}
 
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+		int i = 0;
+		rate=rate/100;
+		for(i = 0; i < n; i++){
+			loan=(loan - payment)*(1+rate);
+		}
+
+		return loan;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,8 +44,16 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+       iterationCounter=0;
+	   double g = loan/n;
+	   System.out.println("g "+g);
+	   while(endBalance(loan, rate, n, g+epsilon) > epsilon)
+	   {
+		g+=epsilon;
+		iterationCounter++;
+	   }
+	   g+=epsilon;
+		return g;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -48,7 +62,20 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+		iterationCounter = 0;
+		double l = loan/n;
+		double h = loan; 
+		 double index = 0.5*(h + l);
+		while(h-l > epsilon) {
+			if(endBalance(loan, rate, n, index)*endBalance(loan, rate, n, l)>0) {
+				l = index;
+				index = 0.5*(h + l);
+			} else {
+				h = index;
+				index = 0.5*(h + l);
+			}
+			iterationCounter++;
+		}
+		return index;
     }
 }
